62173b53bf96004b4f0c7e8355f05013858dd49e
Abrar Alsalman <AbrarAlsalman@hotmail.com>
2025-05-29 00:16:13 +0300

    Initial Commit

---
+++ NYTimes/NYTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYTimesApp.swift
+//  NYTimes
+//
+//  Created by Abrar on 01/12/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct NYTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
